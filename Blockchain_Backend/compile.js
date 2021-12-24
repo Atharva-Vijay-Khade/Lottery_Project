bea87aef90f8c36befa7e957bcf65604fcf5e12b
Atharva Vijay Khade <atharvavk123@gmail.com>
2021-12-24 22:07:34 +0530

    Resolve errors

--- Blockchain_Backend/compile.js
+++ Blockchain_Backend/compile.js
@@ -25,6 +25,18 @@ var output = JSON.parse(solc.compile(JSON.stringify(input)));
 
 var interface = output.contracts["Lottery.sol"]["Lottery"].abi;
 
+// function traverse(interface) {
+//     for (var i in interface) {
+//         if (!!interface[i] && typeof (interface[i]) == "object") {
+//             console.log(interface[i]);
+//             traverse(interface[i]);
+//         } else {
+//             console.log(interface[i]);
+//         }
+//     }
+// }
+// traverse(interface);
+
 var bytecode = output.contracts['Lottery.sol']["Lottery"].evm.bytecode.object;
 
 module.exports = { interface, bytecode };
