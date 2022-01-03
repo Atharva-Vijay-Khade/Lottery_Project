0c87d58537470ed28b24b0a700db0f8ba40b1354
thisisbipin <bipinmjadav087@gmail.com>
2022-01-03 15:52:26 +0530

    logo added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Play from "./components/Play";
 import Clock from "./components/Clock";
 import InfoBanner from "./components/InfoBanner";
 import MainContainer from "./components/MainContainer";
-
+import logo from "./assets/lottery-icon-12.png";
 
 function App() {
   return (
     <>
       <header>
+        <img className="logo-img" src={logo} alt="" />
         <h1>Lottery Project</h1>
       </header>
       <MainContainer />
