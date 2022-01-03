7b6ce68b2a122d1133ac5e6d7e8ea8a55742b4f9
thisisbipin <bipinmjadav087@gmail.com>
2022-01-03 16:25:27 +0530

    updated list style

--- src/components/InfoBanner.jsx
+++ src/components/InfoBanner.jsx
@@ -8,11 +8,18 @@ export default class InfoBanner extends Component {
         <img src={lotteryImage} alt="" />
         <div className="info-container__rules-and-regu">
           <h1>Rules To Participate</h1>
-          <ul>
-            <li>Connect your metamask wallet</li>
-            <li>Set Rinkeby test network to your wallet</li>
-            <li>The entry price of the lottery is 1 ETH, make sure of have sufficient funds in your wallet</li>
-            <li>Click on play and confirm your payment over metamask</li>
+          <ul class="list-group">
+            <li className="list-group-item">◉ Connect your metamask wallet</li>
+            <li className="list-group-item">
+              ◉ Set Rinkeby test network to your wallet
+            </li>
+            <li className="list-group-item">
+              ◉ The entry price of the lottery is 1 ETH, make sure of have
+              sufficient funds in your wallet
+            </li>
+            <li className="list-group-item">
+              ◉ Click on play and confirm your payment over metamask
+            </li>
           </ul>
         </div>
       </div>
